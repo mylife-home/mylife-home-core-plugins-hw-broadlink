7c8c4e7c50fb433d42a32660aee7e785db013705
Vincent Trumpff <vincent@stoic.com>
2017-05-22 16:26:29 +0200

    mp2 device

--- test/exec.js
+++ test/exec.js
@@ -1,9 +1,36 @@
 'use strict';
 
-const Device = require('../lib/service/device');
+const async = require('async');
+const { MP1 } = require('../lib/service/devices');
 
-// smart-plug1
-
-const device = new Device('smart-plug1.mti-team2.dyndns.org');
+const device = new MP1('smart-plug1.mti-team2.dyndns.org');
 
 device.connect();
+
+device.on('state', console.log);
+
+device.once('online', () => {
+  async.series([
+    cb => { console.log('checkState'); device.checkState(); cb(); },
+    createPause(),
+    createTask(1, true),
+    createPause(),
+    createTask(1, false),
+    createTask(2, true),
+    createTask(3, true),
+    createTask(4, true),
+    createPause(),
+    createTask(2, false),
+    createTask(3, false),
+    createTask(4, false),
+  ],
+  (err) => err && console.error(err));
+});
+
+function createPause() {
+  return cb => setTimeout(cb, 2000);
+}
+
+function createTask(id, value) {
+  return cb => { console.log('setState', id, value); device.setState(id, value); cb(); };
+}
